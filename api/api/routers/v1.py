6c5c119e16f5dbe4cbca815150249dae3524ff59
Manuel Raimann <raimannma@outlook.de>
2024-09-24 21:11:23 +0200

    Fix

--- api/api/routers/v1.py
+++ api/api/routers/v1.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from api.globs import CH_CLIENT
 
@@ -6,12 +6,23 @@ router = APIRouter(prefix="/v1")
 
 
 @router.get("/match-score-distribution")
-def get_match_score_distribution():
-    query = """
-    SELECT match_score, COUNT(match_score) as match_score_count
-    FROM active_matches
-    GROUP BY match_score
-    ORDER BY match_score;
-    """
-    result = CH_CLIENT.execute(query)
+def get_match_score_distribution(hero_ids: list[int] | None = Query(None)):
+    if hero_ids is None:
+        query = """
+        SELECT match_score, COUNT(DISTINCT match_id) as match_score_count
+        FROM active_matches
+        GROUP BY match_score
+        ORDER BY match_score;
+        """
+        result = CH_CLIENT.execute(query)
+    else:
+        query = """
+        SELECT match_score, COUNT(DISTINCT match_id) as count
+        FROM active_matches
+        ARRAY JOIN players
+        WHERE `players.hero_id` IN %(hero_ids)s
+        GROUP BY match_score
+        ORDER BY match_score;
+        """
+        result = CH_CLIENT.execute(query, {"hero_ids": hero_ids})
     return [{"match_score": row[0], "count": row[1]} for row in result]
