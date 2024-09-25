54e0bfe9f7b0fa26738ea2d72f00044c443461c2
Manuel Raimann <raimannma@outlook.de>
2024-09-25 19:58:57 +0200

    Do not allow api key header

--- api/api/utils.py
+++ api/api/utils.py
@@ -1,19 +1,13 @@
 from fastapi import HTTPException, Security
-from fastapi.security.api_key import APIKeyHeader, APIKeyQuery
+from fastapi.security.api_key import APIKeyQuery
 from starlette.status import HTTP_403_FORBIDDEN
 
 api_key_query = APIKeyQuery(name="api_key", auto_error=False)
-api_key_header = APIKeyHeader(name="authorization", auto_error=False)
 
 
-async def get_api_key(
-    api_key_header: str = Security(api_key_header),
-    api_key_query: str = Security(api_key_query),
-):
+async def get_api_key(api_key_query: str = Security(api_key_query)):
     with open("api_keys.txt") as f:
         available_api_keys = f.read().splitlines()
-    if api_key_header in available_api_keys:
-        return api_key_header
     if api_key_query in available_api_keys:
-        return api_key_header
+        return api_key_query
     raise HTTPException(status_code=HTTP_403_FORBIDDEN)
